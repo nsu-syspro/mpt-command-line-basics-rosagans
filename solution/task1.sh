5be169d5e40e5bacf583548ff5c81c09ad26e188
rosagans <rom.tebyak@gmail.com>
2025-09-05 20:57:26 +0700

    Update solution

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,6 +1,6 @@
-cd data
 mkdir temp
-tar -xf archive-part1.tar -C temp/
-unzip archive-part2.tar -d temp/
-tar -czf archive-combined.tar.gz -C temp/ .
+tar -xf data/archive-part1.tar -C temp
+unzip data/archive-part2.zip -d temp
+tar -czf data/archive-combined.tar.gz -C temp/ .
 rm -rf temp
+
